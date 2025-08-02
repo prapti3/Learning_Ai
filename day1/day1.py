ed9467f296400faa4b07b98e93a7f94985170549
prapti3 <prapti.chavan3@gmail.com>
2025-08-02 12:58:21 +0530

    add code

--- day1/day1.py
+++ day1/day1.py
@@ -1,1 +1,110 @@
+# simple print statement 
 print("Hello world");
+
+# if-else statements 
+if(5>2):
+    print("five is bigger than 2");
+
+else:
+    print("error");
+
+
+#comment
+'''multi
+line '''
+
+
+
+#Variables: -> containers for storing data values
+x =5;
+y = "HaveFun";
+
+print(x);
+print(y);
+
+#variables dont need any type& can change type after they have been set
+
+
+# casting : if we have to specify the datatype of a variable
+a= str(3);
+b =int(2);
+z= float(3);
+
+
+# get the Types of variables : type() function
+print(type(a));
+print(type(b));
+print(type(z));
+
+# "" and '' both are same
+
+# Case sensitive : variables names are case-sensitive
+
+g=4;
+G="Sally";
+# both are different 
+
+
+# assign multiple variables
+x,y,z = "Orange","Banana","Mango";
+print(x);
+print(y);
+
+
+# x,y,z = "ORange"; == error 
+# equal values and values should be there
+
+
+#list:
+fruits = ["apple","banana","Cherry"];
+x,y,z = fruits;
+print(x);
+print(y);
+
+#output ways
+print(x);
+print(x,y,z);
+print(x+y+z);
+print(x+ " " + y + " "+z);
+
+
+'''
+Global Variables: variables that are created outside of a function
+-> all above variables are global variables 
+-> can be used by everyone, both inside of function & outside
+
+
+
+'''
+
+x = "awesome";
+
+def func():
+    x = "fun code";
+    print("Python is " + x);
+
+func();
+
+print("Python is " + x);
+
+#example
+
+
+#global keyword :  to create variables inside function which are usually local to function 
+# but to create global variables inside function use "global" keyword
+y = "Learning";
+
+def myFun():
+    global y;
+    y = "learn";
+    print(y +" "+ "python");
+
+myFun();
+
+print(y + " " + "python2")
+
+
+# to change values of variables inside function use global keyword 
+# so that variables can be accessed outside the functions
+
+#y = "Learning";
--- day1/notebook.md
+++ day1/notebook.md
@@ -11,4 +11,52 @@
 
 
 3. Why python?
-- 
+- works on different platforms (window, mac, linux etc)
+- simple syntax
+- procedural run, object-oriented or functional way
+
+4. Comments
+- # single line
+- '''''' multi line
+
+5. variables?
+- not need to give any type to the variables & can be changed once declared
+- case-sensitive
+
+** Variable name **
+a. must start with letter or underscore character
+b. names cannot start with number
+c. names only contain alpha-numeric characters and underscore(A-z, 0-9 & _)
+d. no python keywords should be used
+
+
+6. Casting 
+- add str(), int(), float() 
+- check type : type() function 
+
+('' and "" both are same)
+
+
+1. Camel case
+myVariableName="John"
+2. Pascal Case
+MyVariableName="John"
+3. Snake Case
+my_variable_name="John"
+
+7. Ways to Assign values 
+- x=2;
+- x= str(3);
+- x,y,z = 3,4,5;
+
+
+8. global variables : 
+- can be used inside & outside function
+- all above are global functions
+
+9. Local variables:
+- defined inside function are local
+- can only be used inside function
+
+
+
